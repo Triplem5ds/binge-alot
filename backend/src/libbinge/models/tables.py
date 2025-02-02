90ab7d0f9b7ea3e94a7991ac0a4b315d8febfed1
triplem5ds <mmmanany@yahoo.com>
2025-02-02 15:10:43 +0200

    Insert tables

---
+++ backend/src/libbinge/models/tables.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+def create(engine):
+    Base.metadata.create_all(engine)
+
+
+class BingeType(Base):
+    __tablename__ = "binge_type"
+    id_binge_type = Column(Integer)
+    binge_type_code = Column(String)
+    name = Column(String)
+
+    def __repr__(self):
+        return f"<BingeType(id_binge_type='{self.id_binge_type}', binge_type_code='{self.binge_type_code}', name='{self.name}')>"
+
+
+class Binge(Base):
+    __tablename__ = "binge_type"
+    id_binge_type = Column(Integer)
+    id_binge_type = Column(Integer)
+    name = Column(String)
+    binge_code = Column(String)
+    id_parent_binge = Column(Integer, nullable=True)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    status = Column(String)
